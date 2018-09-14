5dcffd6e6d9a7d182142eb0ddf0e2c7d9ee863e2
Kirill Veselov <kirill.veselov@springernature.com>
2018-09-14 11:40:16 +0200

    implement endpoints to fetch * all users data * user data by id

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,14 +1,27 @@
 package controllers
 
 import javax.inject._
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.Json
 import play.api.mvc._
+import service.UserService
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class UserController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class UserController @Inject()(cc: ControllerComponents,
+                               userService: UserService) extends AbstractController(cc) {
 
-  def getUserData(id: Int): Action[AnyContent] = Action {
-    Ok(Json.toJson(JsObject.empty))
+  def getUser(id: Int): Action[AnyContent] = Action.async {
+    userService.getUserData(id).map {
+      case Some(user)=> Ok(Json.toJson(user))
+      case None => NotFound
+    }
   }
 
+  def getAllUsers: Action[AnyContent] = Action.async {
+    userService.getUsersData.map {
+      case Some(users)=> Ok(Json.toJson(users))
+      case None => NotFound
+    }
+  }
 }
