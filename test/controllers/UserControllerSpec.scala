20feae51d6527c426d14d110c16923bb12ced3d9
Kirill Veselov <kirill.veselov@springernature.com>
2018-09-14 15:10:02 +0200

    e2e test for UserController#getAllUsers endpoint

--- test/controllers/UserControllerSpec.scala
+++ test/controllers/UserControllerSpec.scala
@@ -3,7 +3,7 @@ package controllers
 import org.scalatest.MustMatchers
 import org.scalatestplus.play.PlaySpec
 import play.api.http.Status
-import play.api.libs.json.JsArray
+import play.api.libs.json.{JsArray, JsDefined}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
@@ -36,6 +36,26 @@ class UserControllerSpec extends PlaySpec with SpecWithPlayApp with MustMatchers
         )
 
         status(result) mustBe Status.NOT_FOUND
+      }
+    }
+  }
+
+  "UserController#getAllUsers" when {
+    "respond with all users+posts data" in {
+      val result = call(
+        controller.getAllUsers,
+        FakeRequest(method = "GET", path = "/users")
+      )
+
+      status(result) mustBe Status.OK
+
+      val json = contentAsJson(result).as[JsArray]
+      val userIds = json.value.map(user => (user \ "id").as[Int])
+      userIds.size mustBe 10
+      userIds must contain allOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+      json.value.foreach { user =>
+        (user \ "posts") mustBe a[JsDefined]
       }
     }
   }
